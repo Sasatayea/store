468d3376adeaebc63b31829d2683018e4f5c6eec
mostafa salah tayea <memosalah553@gmail.com>
2022-04-30 23:07:36 +0200

    mmm

--- App.js
+++ App.js
@@ -9,16 +9,29 @@ import Login from "./Components/Users/Login";
 import Register from "./Components/Users/Register";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import cart from "./Components/Cart/cart";
 import { getAuth } from "firebase/auth";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 // import Cities from "./Components/Cities/Cities";
 // import GuessMyNumber from "./Components/GuessMyNumber";
 import fpage from './Components/Cities/fpage';
 import product from './Components/items/product';
+import profile from './Components/Cities/profile';
+import search from './Components/Cities/search';
 
-export default function App() {
+function Root() {
+  return (
+    <Stack.Navigator >
+
+    </Stack.Navigator>
+  );
+}
+export default function App({navigation}) {
+
+  
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => setUser(user));
     return () => {
@@ -27,46 +40,22 @@ export default function App() {
   }, []);
   
   const [user, setUser] = useState(undefined);
-//git curant user info
 
-// const auth = getAuth();
-// const userr = auth.currentUser;
-
-// if (userr !== null) {
-//   const email = userr.email;
-//   console.log("ssssssssss",email);
-// }
 console.log(user)
-
-      // user ? <CitiesList /> : <Register/>
-
       if(user){
-            return (
-            <NavigationContainer>
-            <Stack.Navigator >
-              <Stack.Screen name="Home Page"   component={CitiesList}
-                options={{
-                  headerStyle: {
-                    backgroundColor: 'white',
-                  },
-                  headerTitleStyle: {
-                    fontWeight: 'bold',
-                  },
-                  headerRight: () => (
-                      <Button
-                      onPress={() => navigation.navigate('cart')}
-                      title="cart"
-                    /> 
-                    // <Img source={imm}  />
-                  ),
-                }}
-              />
-              <Stack.Screen name="EditCity" component={EditCity} />
-              <Stack.Screen name="product" component={product} />
-              <Stack.Screen name="cart" component={cart} />
-            </Stack.Navigator>
-          </NavigationContainer>
-              // <CitiesList /> 
+            return ( 
+              
+              <NavigationContainer>
+              <Tab.Navigator>
+                  <Tab.Screen name="Home" component={CitiesList}/>
+                  <Tab.Screen name="Cart" component={cart}/>
+                  <Tab.Screen name="Profile" component={profile}/>
+                  <Tab.Screen name="product" component={product}/>
+                  <Tab.Screen name="Search" component={search}/>
+
+              </Tab.Navigator>
+      
+              </NavigationContainer>
               )
       }else{ 
         return(
@@ -76,6 +65,7 @@ console.log(user)
               <Stack.Screen name="Login" component={Login} />
               <Stack.Screen name="Register" component={Register} />
             </Stack.Navigator>
+            
           </NavigationContainer>
             //<fpage />
         )}
