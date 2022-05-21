14b073e1a43066f2543a2ed0b81585edad7d248d
Amr Moustafa <sheeka26122000@gmail.com>
2022-05-21 23:31:00 +0200

    mjh

--- Components/cart/Adress.jsx
+++ Components/cart/Adress.jsx
@@ -18,38 +18,77 @@ import { editUser, getUsers, subscribeUser } from "../../db/Data/Users";
 import AdressItem from "../items/AdressItem";
 export default function Adress({ navigation, route }) {
   const item = route.params;
-  const user1 = item.user1;
+  const cartt = item.cart;
   const total = item.total;
-  const [adress, setAdress] = useState(user1[0].adress);
-  
+  const [adress, setAdress] = useState([]);
   const [isSelected, setSelected] = useState("");
   const [newA, setNewA] = useState("");
+  const auth = getAuth();
+  const userr = auth.currentUser;
+  const getAdress = () => {
+    getUserById(userr.uid).then((user) => {
+      const user1 = user;
+      let adress = user1[0].adress;
+      let adress2 = [];
+      for (let i = 0; i < adress.length; i++) {
+        adress2[i] = { adress: adress[i], isSelected: false };
+      }
+      setAdress(adress2);
+      console.log(adress2);
+    });
+  };
+  useEffect(() => {
+    getAdress();
+  }, []);
 
+  useEffect(() => {
+    const unsubscribeUser = subscribeUser(({ change, snapshot }) => {
+      if (change.type === "added") {
+        getAdress();
+      }
+      if (change.type === "modified") {
+        getAdress();
+      }
+      if (change.type === "removed") {
+        getAdress();
+      }
+    });
+
+    return () => {
+      unsubscribeUser();
+    };
+  }, []);
   const Cash = () => {
-  const carr = user1[0].cart ;
-  const sold = user1[0].sold ;
-  const money = user1[0].money ;
-      addOrder({order:carr,adress:isSelected,client:user1[0],cost:total ,Accept :0});
+    getUserById(userr.uid).then((user) => {
+      const user1 = user;
+      let money = user1[0].money;
+      let sold = user1[0].sold;
+      console.log("soled ", cartt);
+      let carr = cartt;
+      addOrder({order:cartt,adress:isSelected,client:user1[0].name,clientId:user1[0].id,cost:total});
       editUser({
         ...user1[0],
         money: money - total,
         cart: [],
         sold: [...carr, ...sold],
-      }).then(navigation.navigate("Home"));
+      }).then(navigation.navigate("Cart"));
+    });
   };
-
   const AddAdress = () => {
-
-    console.log("El user =>" ,user1[0].adress);
-      let lastaddr = user1[0].adress ;
+    getUserById(userr.uid).then((user) => {
+      const user1 = user;
+      let adress = user1[0].adress;
+      adress[adress.length] = newA;
+      
       editUser({
         ...user1[0],
-        adress:[...lastaddr ,newA],
+        adress: adress,
       });
+    });
   };
-  
   return (
     <View>
+      
       <View style={styles.inpp}>
         <View style={styles.input}>
           <TextInput onChangeText={setNewA} placeholder="New Adress" />
@@ -63,11 +102,11 @@ export default function Adress({ navigation, route }) {
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => (
             <View style={{ flexDirection: "row" }}>
-              <Text>{item}</Text>
+              <Text>{item.adress}</Text>
               <RadioButton
-                value={item}
-                status={isSelected === item ? "checked" : "unchecked"}
-                onPress={() => setSelected(item)}
+                value={item.adress}
+                status={isSelected === item.adress ? "checked" : "unchecked"}
+                onPress={() => setSelected(item.adress)}
               />
             </View>
           )}
