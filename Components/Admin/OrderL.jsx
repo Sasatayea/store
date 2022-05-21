f1287dc3a8b3e26d406dd6afedcf10a4149fd135
Amr Moustafa <sheeka26122000@gmail.com>
2022-05-22 01:11:25 +0200

    constrains

--- Components/Admin/OrderL.jsx
+++ Components/Admin/OrderL.jsx
@@ -1,10 +1,7 @@
-import { View, Text ,  StyleSheet ,Button } from 'react-native'
+import { View, Text ,  StyleSheet ,Button,TouchableOpacity,FlatList } from 'react-native'
 import React from 'react'
 import { deleteOrder, editOrder, getOrders, subscribeOrders } from '../../db/Data/Orders';
-import { useEffect } from 'react';
-import { FlatList } from 'react-native';
-import { useState } from 'react';
-import { TouchableOpacity } from 'react-native-web';
+import { useEffect,useState } from 'react';
 import { addHistory } from '../../db/Data/History';
 
 const OrderL = ({navigation}) => {
--- Components/Pages/UserEditInPro.jsx
+++ Components/Pages/UserEditInPro.jsx
@@ -1,8 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,Button,TextInput } from "react-native";
 import React from "react";
 import { useState } from "react";
-import { TextInput } from "react-native";
-import { Button } from "react-native-web";
 import { editUser, getUserById } from "../../db/Data/Users";
 import { getAuth } from "firebase/auth";
 
@@ -12,6 +10,7 @@ const UserEditInPro = (items) => {
   const auth = getAuth();
   const userr = auth.currentUser;
   const [image, setImage] = useState(item.image);
+  const [background, setBackground] = useState(item.background);
   const [pass, setpass] = useState(item.password);
   const [countryname, setcountryname] = useState(item.countryname);
 
@@ -25,6 +24,7 @@ const UserEditInPro = (items) => {
         image: image,
         password: pass,
         countryname: countryname,
+        background:background,
       });
     });
   };
@@ -61,7 +61,7 @@ const UserEditInPro = (items) => {
           />
         </View>
       </View>
-      <View style={{ flexDirection: "row" }}>
+      
         
         <View style={styles.in}>
           <TextInput
@@ -72,7 +72,16 @@ const UserEditInPro = (items) => {
             style={styles.inpp}
           />
         </View>
-      </View>
+        <View style={styles.in}>
+          <TextInput
+            onChangeText={setBackground}
+            keyboardType="default"
+            placeholder="background"
+            value={background}
+            style={styles.inpp}
+          />
+        </View>
+      
       <View style={{ flexDirection: "row" }}>
         
         <View style={styles.in}>
